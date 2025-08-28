4eed8dd9ace10a5b43eefb76d1177021217df5db
JavierMS <javimesa16@gmail.com>
2025-08-28 11:53:57 -0600

    Prueba con un learning rate de 0.5

--- test.py
+++ test.py
@@ -1,19 +1,5 @@
-"""
-    Testing code for different neural network configurations.
-    Adapted for Python 3.5.2
-
-    Usage in shell:
-        python3.5 test.py
-
-    Network (network.py and network2.py) parameters:
-        2nd param is epochs count
-        3rd param is batch size
-        4th param is learning rate (eta)
-
-    Author:
-        Michał Dobrzański, 2016
-        dobrzanski.michal.daniel@gmail.com
-"""
+#Primer codigo para entrenar la red neuronal
+#Este codigo esta basado en el repositorio de MichalDanielDobrzanski
 #librerias a utilizar 
 import mnist_loader
 import network
@@ -25,16 +11,14 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 #Damos el mensaje que se cargaron los datos
 print(" Datos cargados correctamente")
-'''
-# ---------------------
+
 # - network.py example:
 #import network
 
-'''
 #Parte del codigo que permite entrenar a la red
 
 net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 1.0, test_data=test_data)
+net.SGD(training_data, 30, 10, 0.5, test_data=test_data)
 #guardamos nuestra red
 with open("red_prueba.pkl", "wb") as f:
     pickle.dump(net, f)
